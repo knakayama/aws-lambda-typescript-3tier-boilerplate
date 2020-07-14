55273cbf41910e76d6bde0dd4e9a7c4cfb7b1d7f
knakayama <knakayama.sh@gmail.com>
2020-07-14 21:43:41 +0800

    Fix a typo

--- src/use-cases/i-wanna/say-something.ts
+++ src/use-cases/i-wanna/say-something.ts
@@ -5,13 +5,13 @@ import { MonologueDatabaseDriver } from '@externals/drivers/database/monologue'
 import { Logger } from '@modules/utils/logger'
 
 export class SaySomethingUseCase {
-  constructor(private readonly _bookDatabaseDriver: MonologueDatabaseDriver) {}
+  constructor(private readonly _monologueDatabaseDriver: MonologueDatabaseDriver) {}
 
   async saySomething(monologue: Monologue): Promise<void> {
     const logger = Logger.getLogger()
     logger.info('use-cases')
     try {
-      await this._bookDatabaseDriver.createMonologue(monologue)
+      await this._monologueDatabaseDriver.createMonologue(monologue)
     } catch (error) {
       throw new InternalServerErrorResult(
         ErrorCodes.InternalServerError,
